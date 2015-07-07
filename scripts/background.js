063b0ddad044320cf21546cc20d09c535bd105f3
Sunjay Varma <varma.sunjay@gmail.com>
2015-07-07 16:37:06 -0400

    Editted manifest and added additional files required for chrome app

--- scripts/background.js
+++ scripts/background.js
@@ -1,7 +1,7 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('window.html', {
+  chrome.app.window.create('application.html', {
     'outerBounds': {
-      'width': 400,
+      'width': 600,
       'height': 500
     }
   });
