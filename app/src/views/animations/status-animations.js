eac0d597e6fc72baab8320edfdcc6bf4311c30ba
Sunjay Varma <varma.sunjay@gmail.com>
2015-09-26 16:47:38 -0400

    Better win animation

--- app/src/views/animations/status-animations.js
+++ app/src/views/animations/status-animations.js
@@ -2,17 +2,28 @@ const {SerialProtocolCommandBuilder} = require('../../serial/serial-protocol');
 
 export default class StatusAnimations {
   static playSuccessAnimation(view, completeCallback) {
-    const frames = [
-      // stripId, panelId, intensity, color
-      [0, 3, 50, "success"],
-      [1, 3, 50, "success"],
-      [2, 3, 50, "success"],
-      [0, 4, 100, "success"],
-      [1, 4, 100, "success"],
-      [2, 4, 100, "success"]
-    ];
+    const frames = [];
 
-    StatusAnimations.playAnimation(frames, view, completeCallback);
+    let direction = 1;
+    let lastPanel = null;
+    for (let round = 0; round < 3; round++) {
+      for (let i = 0; i < 10; i++) {
+        const panel = direction === 1 ? i : 10 - i - 1;
+        const panels = [[panel, 100, "success"]];
+        if (lastPanel !== null) {
+          panels.unshift([lastPanel, 0, "success"]);
+        }
+        const frame = () => {
+          return panels;
+        };
+
+        lastPanel = panel;
+        frames.push(frame);
+      }
+      direction *= -1;
+    }
+
+    StatusAnimations.animateStrips(view, ['0', '1', '2'], frames, 50, completeCallback);
   }
 
   static playFailureAnimation(view, completeCallback) {
@@ -29,31 +40,32 @@ export default class StatusAnimations {
     StatusAnimations.playAnimation(frames, view, completeCallback);
   }
 
-  static playAnimation(frames, view, completeCallback) {
+  /**
+   * Animates several strips identically
+   * It is often useful for each frame function to undo its predecessor (don't forget!)
+   * @param {PanelView} view - the panel view to apply this to
+   * @param {String[]} strips - strip IDs
+   * @param {Function[]} frames - functions that return an array of [panelId, intensity, color] for each frame
+   * @param {Number} delay - delay between frames
+   * @param {Function} completeCallback - the function to call when complete
+   */
+  static animateStrips(view, strips, frames, delay, completeCallback) {
     StatusAnimations.clearView(view);
 
     const playFrame = (frameIndex) => {
-      if (frameIndex > 0) {
-        const [stripId, panelId, intensity, color] = frames[frameIndex - 1];
+      const panelsProperties = frames[frameIndex]();
 
-        const commandString = SerialProtocolCommandBuilder.buildPanelSet({
-          stripId: stripId,
-          panelId: panelId,
-          intensity: 0,
-          color: color
-        });
-        view.serialManager.dispatchCommand(commandString);
+      for (let stripId of strips) {
+        for (let [panelId, intensity, color] of panelsProperties) {
+          const commandString = SerialProtocolCommandBuilder.buildPanelSet({
+            stripId: stripId,
+            panelId: panelId,
+            intensity: intensity,
+            color: color
+          });
+          view.serialManager.dispatchCommand(commandString);
+        }
       }
-
-      const [stripId, panelId, intensity, color] = frames[frameIndex];
-
-      const commandString = SerialProtocolCommandBuilder.buildPanelSet({
-        stripId: stripId,
-        panelId: panelId,
-        intensity: 100,
-        color: color
-      });
-      view.serialManager.dispatchCommand(commandString);
 
       let nextMethod;
       if (frameIndex >= frames.length - 1) {
@@ -62,10 +74,10 @@ export default class StatusAnimations {
       else {
         nextMethod = () => playFrame(frameIndex + 1);
       }
-      setTimeout(nextMethod, 300);
+      setTimeout(nextMethod, delay);
     };
 
-    setTimeout(() => playFrame(0), 300);
+    setTimeout(() => playFrame(0), delay);
   }
 
   static clearView(view) {
