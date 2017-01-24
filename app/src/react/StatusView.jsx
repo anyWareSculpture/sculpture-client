dd0bdf02e1528b4ae611a24478be706764ed3add
Marius Kintel <marius@kintel.net>
2017-01-24 15:59:28 -0500

    Center StatusView, add symbols for component icons

--- app/src/react/StatusView.jsx
+++ app/src/react/StatusView.jsx
@@ -6,6 +6,16 @@ import Config from '../config';
 
 const config = new Config();
 
+const symbolMap = {
+  STRIP_A: 'A',
+  STRIP_B: 'B',
+  STRIP_C: 'C',
+  HANDSHAKE_STRIP: 'H',
+  ART_LIGHTS_STRIP: 'P',
+  Handshake: 'V',
+  connected: 'N',
+};
+
 function buildRequiredCommands() {
   const commands = {};
   // All required panels
@@ -13,12 +23,19 @@ function buildRequiredCommands() {
     const stripId = config.LIGHTS[lightId];
     commands[lightId] = SerialProtocolCommandBuilder.build(SerialProtocol.PANEL_SET_COMMAND, {stripId});
   }
-  commands['Disk Reset'] = SerialProtocolCommandBuilder.build(SerialProtocol.DISK_RESET_COMMAND, {});
   commands['Handshake'] = SerialProtocolCommandBuilder.build(SerialProtocol.HANDSHAKE_COMMAND, {});
   return commands;
 }
 
 export default class StatusView extends React.Component {
+  static propTypes = {
+    center: React.PropTypes.bool,
+  }
+
+  static defaultProps = {
+    center: true,
+  }
+
   constructor(props) {
     super(props);
     const buildstate = { connected: 'yellow' };
@@ -35,7 +52,7 @@ export default class StatusView extends React.Component {
   }
 
   updateNetworkStatus(connected) {
-    this.setState({connected});
+    this.setState({connected: connected ? 'green' : 'red'});
   }
 
   componentWillMount() {
@@ -47,16 +64,30 @@ export default class StatusView extends React.Component {
     // FIXME: this.props.app.removeListener() ?
   }
 
+  renderIcons() {
+    const numIcons = Object.keys(this.state).length;
+    return Object.keys(this.state).map((key, idx) => {
+      const angle = idx * 2*Math.PI / numIcons;
+      const xpos = Math.cos(angle)*20;
+      const ypos = Math.sin(angle)*20;
+      return <g key={key} className={`${this.state[key]}-status`} transform={`translate(${xpos}, ${ypos})`}>
+            <circle r="10"/>
+                     <text x="0" y="0" fontSize="10" textAnchor="middle" alignmentBaseline="middle">{symbolMap[key]}</text>
+        </g>;
+    });
+  }
+
   render() {
-   return <svg id="status-view" style={{
-     backgroundColor: "transparent",
-     position: "absolute",
-     width: 400,
-     height: 50,
-     right: 0,
-     top: 0,
-   }}>
-      {Object.keys(this.state).map((key, idx) => <circle key={key} className={`${this.state[key]}-circle`} cx={25+idx*50} cy="25" r="20"/>)}
+    return this.props.center && <svg id="status-view" viewBox="-50 -50 100 100" style={{
+      backgroundColor: "transparent",
+      position: "absolute",
+      width: "100%",
+      height: "100%",
+      right: 0,
+      top: 0,
+      zIndex: 10,
+    }}>
+      {this.renderIcons()}
     </svg>;
   }
 }
