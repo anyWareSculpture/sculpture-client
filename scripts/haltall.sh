c7be7b639747d36b5a52fdb2847b29671d305db2
Marius Kintel <marius@kintel.net>
2017-02-13 14:23:50 -0500

    Shutdown raspberries in parallel

--- scripts/haltall.sh
+++ scripts/haltall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-ssh pi@sculpture1.local sudo halt
-ssh pi@sculpture2.local sudo halt
-ssh pi@sculpture3.local sudo halt
+ssh pi@sculpture1.local sudo halt &
+ssh pi@sculpture2.local sudo halt &
+ssh pi@sculpture3.local sudo halt &
