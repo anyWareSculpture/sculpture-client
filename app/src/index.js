62af4df147ed1ead69b816e4d646327eb3733d73
Marius Kintel <marius@kintel.net>
2017-02-25 14:49:18 -0500

    Allow for config overrides without recompiling bundles

--- app/src/index.js
+++ app/src/index.js
@@ -6,12 +6,14 @@ import ReactDOM from 'react-dom';
 import MainScreen from './react/MainScreen';
 
 import config from './config';
+import localConfig from '../config';
 import SculptureApp from './app';
 
 window.onload = () => {
   const manifest = chrome.runtime.getManifest();
   console.log(`Version: ${manifest.version}`);
 
+  config.applyLocalConfig(localConfig);
   const app = new SculptureApp(config);
   ReactDOM.render(<MainScreen app={app} restart={() => chrome.runtime.reload()}/>, document.getElementById('anyware-root'));
 };
