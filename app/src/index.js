3f5acc776fed90572762275e82bab94224c52bcf
Marius Kintel <marius@kintel.net>
2017-05-07 14:11:09 -0400

    Support per-sculpture overrides in local storage

--- app/src/index.js
+++ app/src/index.js
@@ -8,10 +8,31 @@ import MainScreen from './react/MainScreen';
 import config from './config';
 import SculptureApp from './app';
 
-window.onload = () => {
+// Read all all local storage and overwrite the corresponding values in the given config object
+// Returns a promise
+function applyFromLocalStorage(config) {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get(null, (items) => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      }
+      else {
+        for (let key of Object.keys(items)) {
+          config[key] = items[key];
+        }
+        resolve(true);
+      }
+    });
+  });
+}
+
+window.onload = async () => {
   const manifest = chrome.runtime.getManifest();
   console.log(`Version: ${manifest.version}`);
 
+  // Apply config from Chrome local storage to anyware_config
+  await applyFromLocalStorage(anyware_config);
+  // Apply config from the global variable anyware_config
   config.applyLocalConfig(anyware_config);
   const app = new SculptureApp(config);
   ReactDOM.render(<MainScreen app={app} restart={() => chrome.runtime.reload()}/>, document.getElementById('anyware-root'));
